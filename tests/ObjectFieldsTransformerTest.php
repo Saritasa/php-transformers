28d092a600c5daa645d4134726f4eb720e02978a
Sergey Populov <sergey@saritasa.com>
2017-06-14 20:37:17 +0500

    Update ObjectFieldsTransformerTest

--- tests/ObjectFieldsTransformerTest.php
+++ tests/ObjectFieldsTransformerTest.php
@@ -1,9 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-
 namespace Tests;
 
 use Saritasa\Database\Eloquent\Models\User;
